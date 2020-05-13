72e8c39d7b37133cddf2e122708fb8b64c05951c
Zahniar Adirahman <zahniar88@gmail.com>
2020-05-14 03:28:16 +0700

    add toast sweetalert

--- src/Sweetalert/Notify.php
+++ src/Sweetalert/Notify.php
@@ -1,10 +1,11 @@
 <?php
 namespace Space\Sweetalert;
 
+use Space\Sweetalert\Toast;
 use Space\Sweetalert\Message;
 
 class Notify
 {
     
-    use Message;
+    use Message, Toast;
 }
---
+++ src/Sweetalert/Toast.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Space\Sweetalert;
+
+trait Toast
+{
+    /**
+     * General script
+     */
+    public static function defaultToast()
+    {
+        return "
+           const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                onOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            })
+        ";
+    }
+
+    /**
+     * Show Toast
+     */
+    public static function toastMessage($message, $type = 'success')
+    {
+        return "
+            <script>
+                " . self::defaultToast() . "
+
+                Toast.fire({
+                    icon: '" . $type . "',
+                    title: '" . $message . "'
+                })
+            </script>
+        ";
+    }
+}
